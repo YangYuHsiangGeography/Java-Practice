087ea6e1a0d8b25b35a4c9069b08d0d9c2d8ba96
YangYuHsiangGeography <python.yangyuhsiang@gmail.com>
2024-06-20 15:12:17 +0800

    Delete java_practive/Hello directory

--- java_practive/Hello/src/Hello.java
+++
@@ -1,7 +0,0 @@
-public class Hello{ //類別名稱 要 跟 程式檔案主檔名 一樣（大小寫也要一樣）
-// 類別定義裡建立主要方法
-    public static void main(String[] args){ //注意 String 首字大寫否則無法執行
-        System.out.println("Hello Java from YANG"); //println=print line
-        System.out.println("My First Java Program");
-    }
-}
