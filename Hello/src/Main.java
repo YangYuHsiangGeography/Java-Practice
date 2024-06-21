52e1485172367ae184a50a7ca330672ac3304c0c
YangYuHsiangGeography <python.yangyuhsiang@gmail.com>
2024-06-21 10:39:42 +0800

    Delete Hello directory

--- Hello/src/Main.java
+++
@@ -1,6 +0,0 @@
-public class Main { //類別名稱 要 跟 程式檔案主檔名 一樣（大小寫也要一樣）
-    public static void main(String[] args){   // 類別定義裡建立主要方法  //注意 String 首字大寫否則無法執行
-        System.out.println("Hello and welcome!");
-        System.out.println ("Yang Yu Hsiang's First Java Project");
-    }
-}
